32524412078fbf14be733e21079478c75885ae1c
RKostochko <kostochko.tech@mail.ru>
2021-09-29 22:00:14 +0300

    feat: add demo-particles

--- js/bird.js
+++ js/bird.js
@@ -8,8 +8,9 @@ class Bird {
 		this.weight = 1;
 	}
 	update() {
-		if (this.y > canvas.height - (this.height * 3)) {
-			this.y = canvas.height - (this.height * 3);
+		let curve = Math.sin(angle) * 24;
+		if (this.y > canvas.height - (this.height * 3) + curve) {
+			this.y = canvas.height - (this.height * 3) + curve;
 			this.vy = 0;
 		} else {
 			this.vy += this.weight;
@@ -20,7 +21,7 @@ class Bird {
 			this.y = 0 + this.height;
 			this.vy = 0;
 		}
-		if (spacePressed) this.flap();
+		if (spacePressed && this.y > this.height * 3) this.flap();
 	} 
 	draw() {
 		ctx.fillStyle = 'red';
--- js/main.js
+++ js/main.js
@@ -15,7 +15,9 @@ function animate() {
 	// ctx.fillRect(10, canvas.height -90, 50, 50);
 	bird.update();
 	bird.draw();
+	handleParticles();
 	requestAnimationFrame(animate);
+	angle += 0.12;
 }
 animate();
 
